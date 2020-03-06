3e1fa71acd6a5568e3a49f6591a956d3a44ddff0
ryan <ryan@nml.co.za>
2020-03-06 08:26:52 +0200

    this is mine now

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  title: 'You shouldnt tweet' // Required
+  author: 'Ryan McCartney', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://gosudev.netlify.com/',
+    github: 'https://github.com/RJMcCartney/',
+    twitter: 'https://twitter.com/cesctea',
+    linkedin: 'https://www.linkedin.com/feed/',
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
